5cf6b0c3c6accad2c7181ca967d29fd3d292b722
Mikiya Morisaki <git@morisakimikiya.com>
2025-08-01 23:09:12 +0900

    update: drunk

--- .claude/drunk.sh
+++ .claude/drunk.sh
@@ -1,6 +1,5 @@
 output="$(pwd)/.claude/drunk.txt"
 
-date '+%Y-%m-%d %H:%M:%S' >> $output
+echo -n "$(date '+%Y-%m-%d %H:%M:%S') " >> $output
 cd ~  # hooks に反応して無限ループ防止
-claude -p "以下の文章の泥酔度を10段階で評価して\n1~10の数値のみを出力して（その他は何も出力しないで）\n\n$(jq -r '.prompt')" >> $output &
-echo >> $output
+claude -p "以下の文章の泥酔度を10段階で評価して\n1~10の数値のみを出力して\n\n良い出力例\n3\n悪い出力例\n泥酔度：3\n\n$(jq -r '.prompt')" >> $output; echo >> $output &
